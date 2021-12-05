c4d30a9bbb3573c41a68e8cf43e60b90224e53e0
dan_kucherenko <daniil.ukraine@gmail.com>
2021-12-05 16:53:44 +0200

    Initiate commit

--- HomeWork11/src/StringReplace.java
+++ HomeWork11/src/StringReplace.java
@@ -2,19 +2,44 @@ import java.io.*;
 import java.util.Scanner;
 
 public class StringReplace {
-    private static void replaceString(File file, String string, String replacingString) throws FileNotFoundException {
+    private static void replaceString(File file, String string, String replacingString) throws IOException {
         Scanner sc = new Scanner(file);
+        StringBuilder outputStringBuilder = new StringBuilder();
+        String formattedLine = null;
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
-            String formattedLine;
-
+            formattedLine = line.replaceAll(string, replacingString);
+            outputStringBuilder.append(formattedLine).append('\n');
         }
+        String outputString = outputStringBuilder.toString();
+        fileWriting(file, outputString);
     }
 
     private static void fileWriting(File file, String data) throws IOException {
         PrintWriter pw = new PrintWriter(new FileWriter(file));
         pw.write(data);
         pw.close();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введіть файл для запису: ");
+        String fileName = sc.nextLine();
+
+        System.out.print("Введіть строку для заміни: ");
+        String string = sc.nextLine();
+
+        System.out.print("Введіть строку для заміни обраної строки: ");
+        String replacingString = sc.nextLine();
+
+        File inputFile = new File(fileName);
+        try {
+            replaceString(inputFile, string, replacingString);
+
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e.getMessage());
+            System.exit(1);
+        }
     }
 
 }
