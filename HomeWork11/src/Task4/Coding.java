f905a28465879ebeed58be0916b5873dfd7e58ac
dan_kucherenko <daniil.ukraine@gmail.com>
2021-12-08 22:29:54 +0200

    Initiate commit

---
+++ HomeWork11/src/Task4/Coding.java
@@ -0,0 +1,52 @@
+package Task4;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Coding {
+    private static String readFile(String fileToEncrypt) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileToEncrypt));
+        StringBuilder outputBuilder = new StringBuilder();
+        String text;
+        while ((text = br.readLine()) != null) {
+            outputBuilder.append(text + '\n');
+        }
+        br.close();
+        return outputBuilder.toString();
+    }
+
+    private static String encode(String data) {
+        StringBuilder outputBuilder = new StringBuilder();
+        for (int i = 0; i < data.length() - 1; i++) {
+            char encodedChar = (char) (data.charAt(i) + 1);
+            outputBuilder.append(encodedChar);
+        }
+        return outputBuilder.toString();
+    }
+
+    private static void writeEncodedFile(String output) {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("src/Task4/encoded.txt"));
+            pw.write(output);
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fileEncoding() {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Введіть назву файла для кодування: ");
+            String fileToEncrypt = "src/Task4/" + sc.nextLine();
+
+            writeEncodedFile(encode(readFile(fileToEncrypt)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+        public static void main (String[]args){
+            fileEncoding();
+        }
+    }
---
+++ HomeWork11/src/Task5/Decoding.java
@@ -0,0 +1,52 @@
+package Task5;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Decoding {
+    private static String readFile(String fileToDecode) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileToDecode));
+        StringBuilder outputBuilder = new StringBuilder();
+        String text;
+        while ((text = br.readLine()) != null) {
+            outputBuilder.append(text + '\n');
+        }
+        br.close();
+        return outputBuilder.toString();
+    }
+
+    private static String decode(String data) {
+        StringBuilder outputBuilder = new StringBuilder();
+        for (int i = 0; i < data.length() - 1; i++) {
+            char decodedChar = (char) (data.charAt(i) - 1);
+            outputBuilder.append(decodedChar);
+        }
+        return outputBuilder.toString();
+    }
+
+    private static void writeDecodedFile(String output) {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("src/Task5/decoded.txt"));
+            pw.write(output);
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fileDecoding() {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Введіть назву файла для декодування: ");
+            String fileToDecode = "src/Task5/" + sc.nextLine();
+
+            writeDecodedFile(decode(readFile(fileToDecode)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main (String[]args){
+        fileDecoding();
+    }
+}
