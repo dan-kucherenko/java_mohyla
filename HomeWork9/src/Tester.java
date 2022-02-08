0a7b1b63107df9e39961ab3aaf320beabde54a91
dan_kucherenko <daniil.ukraine@gmail.com>
2022-02-08 18:06:38 +0200

    test

--- HomeWork9/src/Tester.java
+++ HomeWork9/src/Tester.java
@@ -2,14 +2,14 @@ public class Tester {
     public static void main(String[] args) {
         ClassLibrary cl = new ClassLibrary();
 //        cl.nameSurname("Daniil", "Kucherenko");
-        //cl.sq();
-         cl.generalSum();
-    //    cl.oddSum(); --
-        //cl.realSum(); --
-        //cl.pow();    --
-//cl.factorial();      --
-//cl.function();       --
-  //      cl.random();
-   //   cl.primeNumber();
+//        cl.sq();
+//        cl.generalSum();
+//        cl.oddSum();
+//        cl.realSum();
+//        cl.pow();
+//        cl.factorial();
+//        cl.function();
+    //    cl.random();
+        cl.primeNumber();
     }
 }
