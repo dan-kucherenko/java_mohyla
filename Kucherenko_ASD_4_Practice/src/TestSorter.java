cc6b7ad66a4984f9562130814e22c7819bd5336e
dan_kucherenko <daniil.ukraine@gmail.com>
2022-02-14 00:10:01 +0200

    Final commit

--- Kucherenko_ASD_4_Practice/src/TestSorter.java
+++ Kucherenko_ASD_4_Practice/src/TestSorter.java
@@ -5,18 +5,21 @@ public class TestSorter {
     public static void main(String[] args) {
         Film[] filmsList = {new Film("Lord of the rings", 93000000, 2001), new Film("Pulp Fiction", 8500000, 1994),
                 new Film("Lighthouse", 11000000, 2019)};
+        System.out.println("Your array before sorting: \n" + Arrays.toString(filmsList));
         Sorter s = new Sorter();
         s.bubbleSort(filmsList); //budget compairing with bubble sorter
         s.selectionSort(filmsList); //budget compairing with selection sorter
+        System.out.println('\n' + "Your array is sorted by budget: \n" + Arrays.toString(filmsList));
 
         CompareFilmTitle compareFilmTitle = new CompareFilmTitle();
-        //s.bubbleSortComparator(compareFilmTitle, filmsList); //comparing title through bubble sort with comparator
-        //s.selectionSorterComparator(compareFilmTitle, filmsList);//comparing title through selection sort with comparator
+        s.bubbleSortComparator(compareFilmTitle, filmsList); //comparing title through bubble sort with comparator
+        s.selectionSorterComparator(compareFilmTitle, filmsList);//comparing title through selection sort with comparator
+        System.out.println('\n' + "Your array is sorted by title: \n" + Arrays.toString(filmsList));
 
         CompareFilmReleaseYear compareFilmReleaseYear = new CompareFilmReleaseYear();
-        //s.bubbleSortComparator(compareFilmReleaseYear, filmsList); //comparing release year through bubble sort with comparator
+        s.bubbleSortComparator(compareFilmReleaseYear, filmsList); //comparing release year through bubble sort with comparator
         s.selectionSorterComparator(compareFilmReleaseYear, filmsList);//comparing release year through selection sort with comparator
-        System.out.println(Arrays.toString(filmsList));
+        System.out.println('\n' + "Your array is sorted by release year: \n" + Arrays.toString(filmsList));
 
 
     }
