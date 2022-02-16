d3a1ce6187dacc24a9c878036ffe3a9da5511ea2
dan_kucherenko <daniil.ukraine@gmail.com>
2022-02-16 18:52:43 +0200

    Merge sort added

--- Kucherenko_ASD_5_HW/src/Sorter.java
+++ Kucherenko_ASD_5_HW/src/Sorter.java
@@ -29,7 +29,8 @@ public class Sorter {
         quickSort(arr, leftPointer + 1, highIndex);
         return arr;
     }
-    public static Object[] quickSortComparator(Comparator c, Object[]objects, int lowIndex, int highIndex) {
+
+    public static Object[] quickSortComparator(Comparator c, Object[] objects, int lowIndex, int highIndex) {
         if (lowIndex >= highIndex)
             return objects;
         int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
@@ -54,6 +55,84 @@ public class Sorter {
 
         quickSortComparator(c, objects, lowIndex, leftPointer - 1);
         quickSortComparator(c, objects, leftPointer + 1, highIndex);
+        return objects;
+    }
+
+    public static Comparable[] mergeSort(Comparable[] arr) {
+        int length = arr.length;
+        if (length < 2)
+            return arr;
+        int midIndex = length / 2;
+        Comparable[] leftHalfArray = new Comparable[midIndex];          //split the array to 2
+        Comparable[] rightHalfArray = new Comparable[length - midIndex];
+        for (int i = 0; i < midIndex; i++)
+            leftHalfArray[i] = arr[i];  //write each array
+        for (int i = midIndex; i < length; i++)
+            rightHalfArray[i - midIndex] = arr[i];
+        mergeSort(leftHalfArray);
+        mergeSort(rightHalfArray);
+
+        int leftSize = leftHalfArray.length, rightSize = rightHalfArray.length;
+        int i = 0, j = 0, k = 0;
+        while (i < leftSize && j < rightSize) {
+            if (leftHalfArray[i].compareTo(rightHalfArray[j]) <= 0) {
+                arr[k] = leftHalfArray[i];
+                i++;
+            } else {
+                arr[k] = rightHalfArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < leftSize) {
+            arr[k] = leftHalfArray[i];
+            i++;
+            k++;
+        }
+        while (j < rightSize) {
+            arr[k] = rightHalfArray[j];
+            j++;
+            k++;
+        }
+        return arr;
+    }
+
+    public static Object[] mergeSortComparator(Comparator c, Object[] objects) {
+        int length = objects.length;
+        if (length < 2)
+            return objects;
+        int midIndex = length / 2;
+        Object[] leftHalfArray = new Comparable[midIndex];          //split the array to 2
+        Object[] rightHalfArray = new Comparable[length - midIndex];
+        for (int i = 0; i < midIndex; i++)
+            leftHalfArray[i] = objects[i];  //write each array
+        for (int i = midIndex; i < length; i++)
+            rightHalfArray[i - midIndex] = objects[i];
+        mergeSortComparator(c, leftHalfArray);
+        mergeSortComparator(c, rightHalfArray);
+
+        int leftSize = leftHalfArray.length, rightSize = rightHalfArray.length;
+        int i = 0, j = 0, k = 0;
+        while (i < leftSize && j < rightSize) {
+            if (c.compare(leftHalfArray[i], rightHalfArray[j]) <= 0) {
+                objects[k] = leftHalfArray[i];
+                i++;
+            } else {
+                objects[k] = rightHalfArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < leftSize) {
+            objects[k] = leftHalfArray[i];
+            i++;
+            k++;
+        }
+        while (j < rightSize) {
+            objects[k] = rightHalfArray[j];
+            j++;
+            k++;
+        }
         return objects;
     }
 }
--- Kucherenko_ASD_5_HW/src/TestSorter.java
+++ Kucherenko_ASD_5_HW/src/TestSorter.java
@@ -7,22 +7,33 @@ public class TestSorter {
                 new Film("Lighthouse", 11, 2019), new Film("Snatch", 10, 2000)};
         System.out.println("Your array before sorting: \n" + Arrays.toString(filmsList));
         Sorter s = new Sorter();
+        /*To see the working of the program choose the
+        sorter you want to use and comment others*/
 
         s.quickSort(filmsList, 0, filmsList.length - 1);
         System.out.println('\n' + "Your array is sorted by budget through quick sorter: \n" + Arrays.toString(filmsList));
+        s.mergeSort(filmsList);
+        System.out.println('\n' + "Your array is sorted by budget through merge sorter: \n" + Arrays.toString(filmsList));
 
 
         CompareFilmTitle compareFilmTitle = new CompareFilmTitle();
         s.quickSortComparator(compareFilmTitle, filmsList, 0, filmsList.length - 1);
         System.out.println('\n' + "Your array is sorted by title through quick sort: \n" + Arrays.toString(filmsList));
+        s.mergeSortComparator(compareFilmTitle,filmsList);
+        System.out.println('\n' + "Your array is sorted by title through merge sort: \n" + Arrays.toString(filmsList));
+
 
         CompareFilmReleaseYearFromTheOldest compareFilmReleaseYearFromTheOldest = new CompareFilmReleaseYearFromTheOldest();
         s.quickSortComparator(compareFilmReleaseYearFromTheOldest, filmsList, 0, filmsList.length - 1);
         System.out.println('\n' + "Your array is sorted by release year from the oldest through quick sort: \n" + Arrays.toString(filmsList));
+        s.mergeSortComparator(compareFilmReleaseYearFromTheOldest,filmsList);
+        System.out.println('\n' + "Your array is sorted by title through merge sort: \n" + Arrays.toString(filmsList));
 
         CompareFilmReleaseYearFromTheYoungest compareFilmReleaseYearFromTheYoungest = new CompareFilmReleaseYearFromTheYoungest();
         s.quickSortComparator(compareFilmReleaseYearFromTheYoungest, filmsList, 0, filmsList.length - 1);
         System.out.println('\n' + "Your array is sorted by release year from the youngest through quick sort: \n" + Arrays.toString(filmsList));
+        s.mergeSortComparator(compareFilmReleaseYearFromTheYoungest,filmsList);
+        System.out.println('\n' + "Your array is sorted by title through merge sort: \n" + Arrays.toString(filmsList));
 
     }
 }
