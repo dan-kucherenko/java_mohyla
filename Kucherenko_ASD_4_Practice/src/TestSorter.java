5268b6340a8283fbab424c461fd25d67a2cf4364
dan_kucherenko <daniil.ukraine@gmail.com>
2022-02-16 21:32:03 +0200

    Add annotations

--- Kucherenko_ASD_4_Practice/src/TestSorter.java
+++ Kucherenko_ASD_4_Practice/src/TestSorter.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class TestSorter {
     public static void main(String[] args) {
         Film[] filmsList = {new Film("Lord of the rings", 93000000, 2001), new Film("Pulp Fiction", 8500000, 1994),
-                new Film("Lighthouse", 11000000, 2019)};
+                new Film("Lighthouse", 11000000, 2019), new Film("Snatch", 10000000, 2000)};
         System.out.println("Your array before sorting: \n" + Arrays.toString(filmsList));
         Sorter s = new Sorter();
         s.bubbleSort(filmsList); //budget compairing with bubble sorter
@@ -14,18 +14,22 @@ public class TestSorter {
 
         CompareFilmTitle compareFilmTitle = new CompareFilmTitle();
         s.bubbleSortComparator(compareFilmTitle, filmsList); //comparing title through bubble sort with comparator
+        System.out.println('\n' + "Your array is sorted by title through bubble sorter: \n" + Arrays.toString(filmsList));
         s.selectionSorterComparator(compareFilmTitle, filmsList);//comparing title through selection sort with comparator
-        System.out.println('\n' + "Your array is sorted by title: \n" + Arrays.toString(filmsList));
+        System.out.println('\n' + "Your array is sorted by title through selection sorter: \n" + Arrays.toString(filmsList));
 
         CompareFilmReleaseYearFromTheOldest compareFilmReleaseYearFromTheOldest = new CompareFilmReleaseYearFromTheOldest();
         s.bubbleSortComparator(compareFilmReleaseYearFromTheOldest, filmsList); //comparing release year through bubble sort with comparator
+        System.out.println('\n' + "Your array is sorted by release year from the oldest through bubble sorter: \n" + Arrays.toString(filmsList));
         s.selectionSorterComparator(compareFilmReleaseYearFromTheOldest, filmsList);//comparing release year through selection sort with comparator
-        System.out.println('\n' + "Your array is sorted by release year from the oldest: \n" + Arrays.toString(filmsList));
+        System.out.println('\n' + "Your array is sorted by release year from the oldest through selection sorter: \n" + Arrays.toString(filmsList));
 
         CompareFilmReleaseYearFromTheYoungest compareFilmReleaseYearFromTheYoungest = new CompareFilmReleaseYearFromTheYoungest();
         s.bubbleSortComparator(compareFilmReleaseYearFromTheYoungest, filmsList);
+        System.out.println('\n' + "Your array is sorted by release year from the youngest through bubble sort: \n" + Arrays.toString(filmsList));
+
         s.selectionSorterComparator(compareFilmReleaseYearFromTheYoungest, filmsList);
-        System.out.println('\n' + "Your array is sorted by release year from the youngest: \n" + Arrays.toString(filmsList));
+        System.out.println('\n' + "Your array is sorted by release year from the youngest through selection sort: \n" + Arrays.toString(filmsList));
 
 
     }
