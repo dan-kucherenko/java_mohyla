c60bde3b9dbd140b6894f0e88c5110fd2a18f19f
dan_kucherenko <daniil.ukraine@gmail.com>
2022-03-06 19:34:17 +0200

    Commit

--- Kucherenko_ASD_6_Practice/src/Sorter.java
+++ Kucherenko_ASD_6_Practice/src/Sorter.java
@@ -1,11 +1,54 @@
 public class Sorter {
-    public void bubbleSort (int[]numbers){
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - i - 1; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temp;
+    public void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && key < arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void combSort(int[] arr) {
+        int gap = arr.length;
+        boolean swapped = true;
+        while (gap > 1 || swapped) {
+            if (gap > 1)
+                gap = (int) (gap / 1.3);
+            swapped = false;
+            for (int i = 0; i < arr.length - gap; i++) {
+                if (arr[i] > arr[i + gap]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + gap];
+                    arr[i + gap] = temp;
+                    swapped = true;
                 }
             }
         }
--- Kucherenko_ASD_6_Practice/src/TimeTester.java
+++ Kucherenko_ASD_6_Practice/src/TimeTester.java
@@ -1,24 +1,59 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Random;
 
 public class TimeTester {
+
+    private static int[] numbers = new int[8192];
+    private static double res;
+
     public static void main(String[] args) throws IOException {
-        int[] numbers = new int[20];
+        Stopwatch sw = new Stopwatch();
+        Sorter s = new Sorter();
+        PrintWriter printWriter = new PrintWriter(new FileWriter("timeTester.txt"));
+        calculateTimeForBubbleSorting(sw, s, printWriter);
+        calculateTimeForSelectionSorting(sw, s, printWriter);
+        calculateTimeForInsertionSorting(sw, s, printWriter);
+        calculateTimeForCombSorting(sw, s, printWriter);
+
+
+        printWriter.close();
+    }
+
+    private static void generateNewArray(int[] numbers) {
         Random r = new Random();
         for (int i = 0; i < numbers.length; i++) {
-            int randomNum = r.nextInt(10);
+            int randomNum = r.nextInt();
             numbers[i] = randomNum;
         }
-        System.out.println("Ваш масив до сортування: " + Arrays.toString(numbers));
-        Sorter s = new Sorter();
-        Stopwatch sw = new Stopwatch();
-        PrintWriter bw = new PrintWriter(new FileWriter("timeTester.txt"));
-        double res = sw.elapsedTime();
+    }
+
+    private static void calculateTimeForBubbleSorting(Stopwatch sw, Sorter s, PrintWriter printWriter) {
+        generateNewArray(numbers);
         s.bubbleSort(numbers);
-        bw.println("Hi!");
-        System.out.println("Ваш масив після сортування: " + Arrays.toString(numbers));
+        res = sw.elapsedTime();
+        printWriter.println("Час виконання  алгоритму бульбашкового сортування: " + res);
+    }
+
+    private static void calculateTimeForSelectionSorting(Stopwatch sw, Sorter s, PrintWriter printWriter) {
+        generateNewArray(numbers);
+        s.selectionSort(numbers);
+        res = sw.elapsedTime();
+        printWriter.println("Час виконання  алгоритму бульбашкового сортування: " + res);
+    }
+
+    private static void calculateTimeForInsertionSorting(Stopwatch sw, Sorter s, PrintWriter printWriter) {
+        generateNewArray(numbers);
+        s.insertionSort(numbers);
+        res = sw.elapsedTime();
+        printWriter.println("Час виконання  алгоритму бульбашкового сортування: " + res);
+    }
+
+    private static void calculateTimeForCombSorting(Stopwatch sw, Sorter s, PrintWriter printWriter) {
+        generateNewArray(numbers);
+        s.combSort(numbers);
+        res = sw.elapsedTime();
+        printWriter.println("Час виконання  алгоритму бульбашкового сортування: " + res);
     }
 }
