5fd4377199b8610e2ac79d8c3be25df4dc71468a
dan_kucherenko <daniil.ukraine@gmail.com>
2022-03-16 18:15:37 +0200

    Final commit

--- Kucherenko_ASD_6_Practice/src/CreateAndCalculate.java
+++ Kucherenko_ASD_6_Practice/src/CreateAndCalculate.java
@@ -1,85 +1,85 @@
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-public class CalculatingTime {
-    private static double res;
-    private static PrintWriter printWriter;
-    private static ArrayList<int[]> arrayList = new ArrayList<>();
-    private static int[] arrayLength = {1024, 2048, 4096, 8192, 16384, 32768};
+public class CreateAndCalculate {
+    private double res;
+    private ArrayList<int[]> arrayList = new ArrayList<>();
+    private final int[] ARRAY_LENGTH = {1024, 2048, 4096, 8192, 16384, 32768};
+    private final PrintWriter printWriter = TimeTester.printWriter;
 
     public void calculateTimeForBubbleSorting() {
         printWriter.print("Bubble sort: ");
-        for (int i = 0; i < arrayLength.length; i++) {
+        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
             Stopwatch swBubble = new Stopwatch();
             Sorter.bubbleSort(arrayList.get(i));
             res = swBubble.elapsedTime();
-            printWriter.print("{" + arrayLength[i] + "=" + res + "} ");
+            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
         }
         printWriter.print('\n');
     }
 
     public void calculateTimeForSelectionSorting() {
         printWriter.print("Selection sort: ");
-        for (int i = 0; i < arrayLength.length; i++) {
+        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
             Stopwatch swSelection = new Stopwatch();
             Sorter.selectionSort(arrayList.get(i));
             res = swSelection.elapsedTime();
-            printWriter.print("{" + arrayLength[i] + "=" + res + "} ");
+            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
         }
         printWriter.print('\n');
     }
 
     public void calculateTimeForInsertionSorting() {
         printWriter.print("Insertion sort: ");
-        for (int i = 0; i < arrayLength.length; i++) {
+        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
             Stopwatch swInsertion = new Stopwatch();
             Sorter.insertionSort(arrayList.get(i));
             res = swInsertion.elapsedTime();
-            printWriter.print("{" + arrayLength[i] + "=" + res + "} ");
+            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
         }
         printWriter.print('\n');
     }
 
     public void calculateTimeForCombSorting() {
         printWriter.print("Comb sort: ");
-        for (int i = 0; i < arrayLength.length; i++) {
+        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
             Stopwatch swComb = new Stopwatch();
             Sorter.combSort(arrayList.get(i));
             res = swComb.elapsedTime();
-            printWriter.print("{" + arrayLength[i] + "=" + res + "} ");
+            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
         }
         printWriter.print('\n');
     }
 
     public void calculateTimeForShellSorting() {
         printWriter.print("Shell sort: ");
-        for (int i = 0; i < arrayLength.length; i++) {
+        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
             Stopwatch swShell = new Stopwatch();
             Sorter.shellSort(arrayList.get(i));
             res = swShell.elapsedTime();
-            printWriter.print("{" + arrayLength[i] + "=" + res + "} ");
+            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
         }
         printWriter.print('\n');
     }
 
     public void calculateTimeForQuickSorting() {
         printWriter.print("Quick sort: ");
-        for (int i = 0; i < arrayLength.length; i++) {
+        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
             Stopwatch swQuick = new Stopwatch();
             Sorter.quickSortGeneral(arrayList.get(i));
             res = swQuick.elapsedTime();
-            printWriter.print("{" + arrayLength[i] + "=" + res + "} ");
+            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
         }
         printWriter.print('\n');
     }
 
     public void calculateTimeForMergeSorting() {
         printWriter.print("Merge sort: ");
-        for (int i = 0; i < arrayLength.length; i++) {
+        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
             Stopwatch swMerge = new Stopwatch();
             Sorter.mergeSort(arrayList.get(i));
             res = swMerge.elapsedTime();
-            printWriter.print("{" + arrayLength[i] + "=" + res + "} ");
+            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
         }
         printWriter.print('\n');
     }
@@ -92,5 +92,24 @@ public class CalculatingTime {
         calculateTimeForShellSorting();
         calculateTimeForQuickSorting();
         calculateTimeForMergeSorting();
+    }
+
+    public void createRandomArrays() {
+        for (int j : ARRAY_LENGTH) arrayList.add(GenerateAnArray.generateNewRandomArray(j));
+    }
+
+    public void createSortedArrays() {
+        arrayList.clear();
+        for (int j : ARRAY_LENGTH) arrayList.add(GenerateAnArray.generateSortedArray(j));
+    }
+
+    public void createReverseSortedArray() {
+        arrayList.clear();
+        for (int j : ARRAY_LENGTH) arrayList.add(GenerateAnArray.generateReverseSortedArray(j));
+    }
+
+    public void createArraysWithSameElement() {
+        arrayList.clear();
+        for (int j : ARRAY_LENGTH) arrayList.add(GenerateAnArray.generateNewArrayWithSameElement(j));
     }
 }
--- Kucherenko_ASD_6_Practice/src/TimeTester.java
+++ Kucherenko_ASD_6_Practice/src/TimeTester.java
@@ -2,30 +2,31 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 public class TimeTester {
+    public static PrintWriter printWriter;
 
     public static void main(String[] args) {
         try {
-            PrintWriter printWriter = new PrintWriter(new FileWriter("timeTester.txt"));
-            CreatingArrays ca = new CreatingArrays();
-            CalculatingTime ct = new CalculatingTime();
+            printWriter = new PrintWriter(new FileWriter("timeTester.txt"));
+            CreateAndCalculate ct = new CreateAndCalculate();
 
             printWriter.println("Random arrays:\n");
-            ca.createRandomArrays();   //calculate algorithm time execution for arrays with random numbers
+            ct.createRandomArrays();   //calculate algorithm time execution for arrays with random numbers
             ct.calculateTimeForAllAlgorithms();
 
             printWriter.println("\nSorted arrays:\n");
-            ca.createSortedArrays();   //calculate algorithm time execution for sorted arrays
+            ct.createSortedArrays();   //calculate algorithm time execution for sorted arrays
             ct.calculateTimeForAllAlgorithms();
 
             printWriter.println("\nReverse sorted arrays:\n");
-            ca.createReverseSortedArray();   //calculate algorithm time execution for reverse sorted arrays
+            ct.createReverseSortedArray();   //calculate algorithm time execution for reverse sorted arrays
             ct.calculateTimeForAllAlgorithms();
 
             printWriter.println("\nArrays with the same element:\n");
-            ca.createArraysWithSameElement();  //calculate algorithm time execution for arrays with same numbers
+            ct.createArraysWithSameElement();  //calculate algorithm time execution for arrays with same numbers
             ct.calculateTimeForAllAlgorithms();
 
             printWriter.close();
+
         } catch (Exception e) {
             System.out.println("Error " + e);
         }
