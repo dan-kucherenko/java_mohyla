3e297297afd388adf32ac9f97021fa0037cacd4b
dan_kucherenko <daniil.ukraine@gmail.com>
2022-03-16 17:11:49 +0200

    Final commit

--- Kucherenko_ASD_6_Practice/src/TimeTester.java
+++ Kucherenko_ASD_6_Practice/src/TimeTester.java
@@ -1,8 +1,7 @@
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class TimeTester {
     private static double res;
