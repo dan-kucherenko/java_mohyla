30787dd99d5ecb7f18d69d8077cef4e9f03a21f7
dan_kucherenko <daniil.ukraine@gmail.com>
2022-04-07 13:44:18 +0300

    Refactored code

--- Kucherenko_ASD_6_Practice/src/CreateAndCalculate.java
+++ Kucherenko_ASD_6_Practice/src/CreateAndCalculate.java
@@ -6,92 +6,162 @@ public class CreateAndCalculate {
     private ArrayList<int[]> arrayList = new ArrayList<>();
     private final int[] ARRAY_LENGTH = {1024, 2048, 4096, 8192, 16384, 32768};
     private final PrintWriter printWriter = TimeTester.printWriter;
+    private Stopwatch sw;
 
-    public void calculateTimeForBubbleSorting() {
-        printWriter.print("Bubble sort: ");
-        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
-            Stopwatch swBubble = new Stopwatch();
-            Sorter.bubbleSort(arrayList.get(i));
-            res = swBubble.elapsedTime();
-            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+    class CalculateTime {
+        public void calculateTime() {
+            sw = new Stopwatch();
+            for (int i = 0; i < ARRAY_LENGTH.length; i++) {
+                sw.startTime();
+                Sorter.bubbleSort(arrayList.get(i));
+                res = sw.elapsedTime();
+                printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+            }
+            printWriter.print('\n');
         }
-        printWriter.print('\n');
     }
 
-    public void calculateTimeForSelectionSorting() {
-        printWriter.print("Selection sort: ");
-        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
-            Stopwatch swSelection = new Stopwatch();
-            Sorter.selectionSort(arrayList.get(i));
-            res = swSelection.elapsedTime();
-            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+
+    class CalculateTimeForBubbleSorting extends CalculateTime {
+        public void calculateTimeForBubbleSorting() {
+            printWriter.print("Bubble sort: ");
+            calculateTime();
         }
-        printWriter.print('\n');
     }
 
-    public void calculateTimeForInsertionSorting() {
-        printWriter.print("Insertion sort: ");
-        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
-            Stopwatch swInsertion = new Stopwatch();
-            Sorter.insertionSort(arrayList.get(i));
-            res = swInsertion.elapsedTime();
-            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+    class CalculateTimeForSelectionSorting extends CalculateTime {
+        public void calculateTimeForSelectionSorting() {
+            printWriter.print("Selection sort: ");
+            calculateTime();
         }
-        printWriter.print('\n');
     }
 
-    public void calculateTimeForCombSorting() {
-        printWriter.print("Comb sort: ");
-        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
-            Stopwatch swComb = new Stopwatch();
-            Sorter.combSort(arrayList.get(i));
-            res = swComb.elapsedTime();
-            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+    class CalculateTimeForInsertionSorting extends CalculateTime {
+        public void calculateTimeForInsertionSorting() {
+            printWriter.print("Insertion sort: ");
+            calculateTime();
         }
-        printWriter.print('\n');
     }
 
-    public void calculateTimeForShellSorting() {
-        printWriter.print("Shell sort: ");
-        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
-            Stopwatch swShell = new Stopwatch();
-            Sorter.shellSort(arrayList.get(i));
-            res = swShell.elapsedTime();
-            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+    class CalculateTimeForCombSorting extends CalculateTime {
+        public void calculateTimeForCombSorting() {
+            printWriter.print("Comb sort: ");
+            calculateTime();
         }
-        printWriter.print('\n');
     }
 
-    public void calculateTimeForQuickSorting() {
-        printWriter.print("Quick sort: ");
-        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
-            Stopwatch swQuick = new Stopwatch();
-            Sorter.quickSortGeneral(arrayList.get(i));
-            res = swQuick.elapsedTime();
-            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+    class CalculateTimeForShellSorting extends CalculateTime {
+        public void calculateTimeForShellSorting() {
+            printWriter.print("Shell sort: ");
+            calculateTime();
         }
-        printWriter.print('\n');
     }
 
-    public void calculateTimeForMergeSorting() {
-        printWriter.print("Merge sort: ");
-        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
-            Stopwatch swMerge = new Stopwatch();
-            Sorter.mergeSort(arrayList.get(i));
-            res = swMerge.elapsedTime();
-            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+    class CalculateTimeForQuickSorting extends CalculateTime {
+        public void calculateTimeForQuickSorting() {
+            printWriter.print("Quick sort: ");
+            calculateTime();
         }
-        printWriter.print('\n');
     }
+
+    class CalculateTimeForMergeSortin extends CalculateTime {
+        public void calculateTimeForMergeSorting() {
+            printWriter.print("Merge sort: ");
+            calculateTime();
+        }
+    }
+//        public void calculateTimeForBubbleSorting() {
+//        printWriter.print("Bubble sort: ");
+//        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
+//            Stopwatch swBubble = new Stopwatch();
+//            Sorter.bubbleSort(arrayList.get(i));
+//            res = swBubble.elapsedTime();
+//            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+//        }
+//        printWriter.print('\n');
+//    }
+
+//    public void calculateTimeForSelectionSorting() {
+//        printWriter.print("Selection sort: ");
+//        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
+//            Stopwatch swSelection = new Stopwatch();
+//            Sorter.selectionSort(arrayList.get(i));
+//            res = swSelection.elapsedTime();
+//            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+//        }
+//        printWriter.print('\n');
+//    }
+
+//    public void calculateTimeForInsertionSorting() {
+//        printWriter.print("Insertion sort: ");
+//        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
+//            Stopwatch swInsertion = new Stopwatch();
+//            Sorter.insertionSort(arrayList.get(i));
+//            res = swInsertion.elapsedTime();
+//            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+//        }
+//        printWriter.print('\n');
+//    }
+
+//    public void calculateTimeForCombSorting() {
+//        printWriter.print("Comb sort: ");
+//        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
+//            Stopwatch swComb = new Stopwatch();
+//            Sorter.combSort(arrayList.get(i));
+//            res = swComb.elapsedTime();
+//            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+//        }
+//        printWriter.print('\n');
+//    }
+
+//    public void calculateTimeForShellSorting() {
+//        printWriter.print("Shell sort: ");
+//        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
+//            Stopwatch swShell = new Stopwatch();
+//            Sorter.shellSort(arrayList.get(i));
+//            res = swShell.elapsedTime();
+//            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+//        }
+//        printWriter.print('\n');
+//    }
+
+//    public void calculateTimeForQuickSorting() {
+//        printWriter.print("Quick sort: ");
+//        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
+//            Stopwatch swQuick = new Stopwatch();
+//            Sorter.quickSortGeneral(arrayList.get(i));
+//            res = swQuick.elapsedTime();
+//            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+//        }
+//        printWriter.print('\n');
+//    }
+
+//    public void calculateTimeForMergeSorting() {
+//        printWriter.print("Merge sort: ");
+//        for (int i = 0; i < ARRAY_LENGTH.length; i++) {
+//            Stopwatch swMerge = new Stopwatch();
+//            Sorter.mergeSort(arrayList.get(i));
+//            res = swMerge.elapsedTime();
+//            printWriter.print("{" + ARRAY_LENGTH[i] + "=" + res + "} ");
+//        }
+//        printWriter.print('\n');
+//    }
 
     public void calculateTimeForAllAlgorithms() {
-        calculateTimeForBubbleSorting();
-        calculateTimeForSelectionSorting();
-        calculateTimeForInsertionSorting();
-        calculateTimeForCombSorting();
-        calculateTimeForShellSorting();
-        calculateTimeForQuickSorting();
-        calculateTimeForMergeSorting();
+        CalculateTimeForBubbleSorting calcBubble = new CalculateTimeForBubbleSorting();
+        calcBubble.calculateTimeForBubbleSorting();
+        CalculateTimeForSelectionSorting calcSelect = new CalculateTimeForSelectionSorting();
+        calcSelect.calculateTimeForSelectionSorting();
+        CalculateTimeForInsertionSorting calcInsertion = new CalculateTimeForInsertionSorting();
+        calcInsertion.calculateTimeForInsertionSorting();
+        CalculateTimeForCombSorting calcComb = new CalculateTimeForCombSorting();
+        calcComb.calculateTimeForCombSorting();
+        CalculateTimeForShellSorting calcShell = new CalculateTimeForShellSorting();
+        calcShell.calculateTimeForShellSorting();
+        CalculateTimeForQuickSorting calcQuick = new CalculateTimeForQuickSorting();
+        calcQuick.calculateTimeForQuickSorting();
+        CalculateTimeForMergeSortin calcMerge = new CalculateTimeForMergeSortin();
+        calcMerge.calculateTimeForMergeSorting();
     }
 
     public void createRandomArrays() {
