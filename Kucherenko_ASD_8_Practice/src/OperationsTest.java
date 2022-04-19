f217dff5dc2fd33a961ce7bae2135be6c041b1ec
dan_kucherenko <daniil.ukraine@gmail.com>
2022-04-19 18:00:46 +0300

    Commit

--- Kucherenko_ASD_8_Practice/src/OperationsTest.java
+++ Kucherenko_ASD_8_Practice/src/OperationsTest.java
@@ -1,19 +1,28 @@
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 public class OperationsTest {
     public static void main(String[] args) {
-        Integer[] set_1 = new Integer[100], set_2 = new Integer[100];
         LinkedHashSet<Integer> result_set = new LinkedHashSet<>();
+        LinkedHashSet<Integer> set_1 = new LinkedHashSet<>();
+        LinkedHashSet<Integer> set_2 = new LinkedHashSet<>();
         OperationsWithSets ows = new OperationsWithSets();
         ows.addRandomElements(set_1, set_2);
-        System.out.println("Union: " + ows.union(set_1, set_2, result_set));
+        System.out.println("Set 1: " + set_1);
+        System.out.println("Set 2: " + set_2);
+//        System.out.println("Union: " + ows.union(set_1, set_2, result_set));
         System.out.println("Intersection: " + ows.intersection(set_1, set_2, result_set));
-        System.out.println("Difference: " + ows.difference(set_1, set_2, result_set));
+//        System.out.println("Difference: " + ows.difference(set_1, set_2, result_set));
         System.out.println("Merge: " + ows.merge(set_1, set_2, result_set));
-        System.out.println("Member: " + ows.member(2, result_set));
-        System.out.println("Make null: " + ows.makeNull(result_set));
-        System.out.println("Insert: " + ows.insert(3, result_set));
-        System.out.println("Remove: " + ows.remove(4, result_set));
+//        System.out.println("Member: " + ows.member(2, set_1));
+//        System.out.println("Make null: " + ows.makeNull(set_1));
+//        System.out.println("Insert: " + ows.insert(3, set_2));
+        System.out.println("Remove: " + ows.remove(4, set_1));
         System.out.println("Assign: " + ows.assign(set_1, set_2));
+//        System.out.println("Min: " + ows.min(set_1));
+//        System.out.println("Max: " + ows.max(set_2));
+        System.out.println("Equal: " + ows.equal(set_1, set_2));
+        System.out.println("Find: " + ows.find(5, set_1, set_2));
+
     }
 }
--- Kucherenko_ASD_8_Practice/src/OperationsWithSets.java
+++ Kucherenko_ASD_8_Practice/src/OperationsWithSets.java
@@ -1,48 +1,64 @@
-import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Random;
 
 public class OperationsWithSets {
     private final int ARRAY_LENGTH = 100;
+    private LinkedHashSet<Integer> intersection_result;
 
-    void addRandomElements(Integer[] set_1, Integer[] set_2) {
+    void addRandomElements(LinkedHashSet<Integer> set_1, LinkedHashSet<Integer> set_2) {
+        Integer[] baseArray = new Integer[ARRAY_LENGTH];
+        Integer[] booleanSet1 = new Integer[ARRAY_LENGTH];
+        Integer[] booleanSet2 = new Integer[ARRAY_LENGTH];
+        for (int i = 0; i < 100; i++)
+            baseArray[i] = i + 1;
         Random r = new Random();
         for (int i = 0; i < ARRAY_LENGTH; i++) {
-            set_1[i] = r.nextInt(2);
-            set_2[i] = r.nextInt(2);
+            booleanSet1[i] = r.nextInt(2);
+            booleanSet2[i] = r.nextInt(2);
+        }
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (booleanSet1[i] == 1)
+                set_1.add(baseArray[i]);
+            else if (booleanSet2[i] == 1)
+                set_2.add(baseArray[i]);
         }
     }
 
-    LinkedHashSet<Integer> union(Integer[] set_1, Integer[] set_2, LinkedHashSet<Integer> result) {
-        result.addAll(Arrays.asList(set_1));
-        result.addAll(Arrays.asList(set_2));
+    LinkedHashSet<Integer> union(LinkedHashSet<Integer> set_1, LinkedHashSet<Integer> set_2,
+                                 LinkedHashSet<Integer> result) {
+        result.addAll(set_1);
+        result.addAll(set_2);
         return result;
 
     }
 
-    LinkedHashSet<Integer> intersection(Integer[] set_1, Integer[] set_2, LinkedHashSet<Integer> result) {
+    LinkedHashSet<Integer> intersection(LinkedHashSet<Integer> set_1, LinkedHashSet<Integer> set_2,
+                                        LinkedHashSet<Integer> result) {
         result.clear();
-        result.addAll(Arrays.asList(set_1));
-        result.retainAll(Arrays.asList(set_2));
+        result.addAll(set_1);
+        result.retainAll(set_2);
         return result;
     }
 
-    LinkedHashSet<Integer> difference(Integer[] set_1, Integer[] set_2, LinkedHashSet<Integer> result) {
+    LinkedHashSet<Integer> difference(LinkedHashSet<Integer> set_1, LinkedHashSet<Integer> set_2,
+                                      LinkedHashSet<Integer> result) {
         result.clear();
-        result.addAll(Arrays.asList(set_1));
-        result.removeAll(Arrays.asList(set_2));
+        result.addAll(set_1);
+        result.removeAll(set_2);
         return result;
     }
 
-    LinkedHashSet<Integer> merge(Integer[] set_1, Integer[] set_2, LinkedHashSet<Integer> result) {
-        if (intersection(set_1, set_2, result) == null)
+    LinkedHashSet<Integer> merge(LinkedHashSet<Integer> set_1, LinkedHashSet<Integer> set_2,
+                                 LinkedHashSet<Integer> result) {
+        result.clear();
+        if (intersection_result == null)
             union(set_1, set_2, result);
         return result;
     }
 
-    boolean member(Integer x, LinkedHashSet<Integer> result) {
+    boolean member(Integer x, LinkedHashSet<Integer> set) {
         boolean hasX = false;
-        if (result.contains(x))
+        if (set.contains(x))
             hasX = true;
         return hasX;
     }
@@ -57,45 +73,43 @@ public class OperationsWithSets {
         return result;
     }
 
-    LinkedHashSet<Integer> remove(Integer x, LinkedHashSet<Integer> result) {
-        result.remove(x);
-        return result;
+    LinkedHashSet<Integer> remove(Integer x, LinkedHashSet<Integer> set) {
+        if (set.contains(x))
+            set.remove(x);
+        else
+            System.out.println("Your set doesn't have the decimal you have inserted");
+        return set;
     }
 
-    Integer[] assign(Integer[] set_1, Integer[] set_2) {
+    LinkedHashSet<Integer> assign(LinkedHashSet<Integer> set_1, LinkedHashSet<Integer> set_2) {
         set_1 = set_2;
         return set_1;
     }
 
-    Integer min(Integer[] set) { //finish later
+    Integer min(LinkedHashSet<Integer> set) { //finish later
         int min = 0;
         return min;
     }
 
-    Integer max(Integer[] set) { //finish later
+    Integer max(LinkedHashSet<Integer> set) { //finish later
         int max = 0;
         return max;
     }
 
-    boolean equal(Integer[] set_1, Integer[] set_2) {
-        boolean isEqual = false;
-        if (set_1 == set_2)
-            isEqual = true;
-        return isEqual;
+    boolean equal(LinkedHashSet<Integer> set_1, LinkedHashSet<Integer> set_2) {
+        return set_1 == set_2;
     }
 
-    String find(Integer x, Integer[] set_1, Integer[] set_2) {
-        String nameOfSet = null;
-        LinkedHashSet<Integer> set1AsAList = new LinkedHashSet<>();
-        set1AsAList.addAll(Arrays.asList(set_1));
-        LinkedHashSet<Integer> set2AsAList = new LinkedHashSet<>();
-        set2AsAList.addAll(Arrays.asList(set_2));
-        if(set1AsAList.contains(x))
-            return "Set_1 contains the decimal you have inserted";
-        else if (set2AsAList.contains(x))
-            return "Set_2 contains the decimal you have inserted";
-        else
-        return "None sets contains the decimal you have inserted";
+    String find(Integer x, LinkedHashSet<Integer> set_1, LinkedHashSet<Integer> set_2) {
+        if (intersection_result != null) {
+            if (set_1.contains(x))
+                return "Set_1 contains the decimal you have inserted";
+            else if (set_2.contains(x))
+                return "Set_2 contains the decimal you have inserted";
+            else
+                return "None sets contains the decimal you have inserted";
+        } else
+            return "Your sets have intersection";
     }
 
 }
