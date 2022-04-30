d590dcd482b3676e4daaeee89e1b1020cf903afa
dan_kucherenko <daniil.ukraine@gmail.com>
2022-04-30 21:48:02 +0300

    Updated/checked test-class

--- Kucherenko_ASD_8_HW/src/Test.java
+++ Kucherenko_ASD_8_HW/src/Test.java
@@ -1,16 +1,18 @@
 public class Test {
     public static void main(String[] args) {
+        MyList<Integer> result = new MyList<>();
         MyList<Integer> myList1 = new MyList<>();
         myList1.add(1);
         myList1.add(2);
         myList1.add(4);
         MyList<Integer> myList2 = new MyList<>();
-//        myList2.add(2);
+        myList2.add(2);
         myList2.add(6);
         myList2.add(5);
         myList2.add(9);
         myList2.add(8);
-        MyList<Integer> result = new MyList<>();
+
+
         MySet<Integer> mySet1 = new MySet<>(myList1);
         MySet<Integer> mySet2 = new MySet<>(myList2);
         MySet<Integer> resultSet = new MySet<>(result);
@@ -27,8 +29,8 @@ public class Test {
         System.out.println("Assign: " + resultSet.assign(mySet1, mySet2));
         System.out.println("Min: " + resultSet.min(mySet2));
         System.out.println("Max: " + resultSet.max(mySet2));
-        System.out.println("Equal: "+ resultSet.equal(mySet1,mySet2));
-        System.out.println("Find: "+ resultSet.find(4,mySet1,mySet2));
+        System.out.println("Equal: " + resultSet.equal(mySet1, mySet2));
+        System.out.println("Find: " + resultSet.find(4, mySet1, mySet2));
 
 
     }
