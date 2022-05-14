2fa7eb40b559e04cf3abfa23e04e4dae1265e23c
dan_kucherenko <daniil.ukraine@gmail.com>
2022-05-14 11:51:18 +0300

    Commit

--- Kucherenko_ASD_10_HW/src/MyGraph.java
+++ Kucherenko_ASD_10_HW/src/MyGraph.java
@@ -1,20 +1,23 @@
 public class MyGraph {
     private MyList[] adjacencyList;
     private int[][] incidenceMatrix;
-    private int numberOfVertices;
+    private int numOfColumns;
+    private int numOfRows;
 
     public MyGraph(int[][] incidenceMatrix) {
-        this.adjacencyList = adjacencyList;
-        numberOfVertices = incidenceMatrix.length;
+        this.incidenceMatrix = incidenceMatrix;
+        this.numOfColumns = incidenceMatrix.length;
+        this.numOfRows = incidenceMatrix[numOfColumns - 1].length;
+        makeEdges();
     }
 
-    private void addIncidentVertices() {
-        adjacencyList = new MyList[numberOfVertices];
-        for (int i = 0; i < numberOfVertices; i++) {
-            adjacencyList[i] = new MyList<Integer>();
-            for (int j = 0; j < numberOfVertices; j++) {
-                if (incidenceMatrix[i][j] == 1)
-                    adjacencyList[i].add(j + 1);
+    private void makeEdges() {
+        adjacencyList = new MyList[numOfRows];
+        for (int column = 0; column < numOfRows; column++) {
+            adjacencyList[column] = new MyList<Integer>();
+            for (int row = 0; row < numOfColumns; row++) {
+                if (incidenceMatrix[row][column] == 1)
+                    adjacencyList[column].add(row + 1);
             }
         }
     }
@@ -22,6 +25,8 @@ public class MyGraph {
     @Override
     public String toString() {
         String result = "";
+        for (int i = 0; i < numOfRows; i++)
+            result += (i + 1) + " -> " + adjacencyList[i].toString() + '\n';
         return result;
     }
 
