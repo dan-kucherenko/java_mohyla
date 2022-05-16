07684271572740a4a0afc8742a862a191f8bb331
dan_kucherenko <daniil.ukraine@gmail.com>
2022-05-16 18:57:11 +0300

    Completed array implementation of binary tree

--- Kucherenko_ASD_11_Practice/src/BinaryTreeForArrRealisation.java
+++ Kucherenko_ASD_11_Practice/src/BinaryTreeForArrRealisation.java
@@ -1,64 +1,78 @@
 public class BinaryTreeForArrRealisation<T> {
-    private T[] arr;
-    private TreeNode root;
-    private int length;
+    private T[] arr = (T[]) new Object[1];
 
-    public BinaryTreeForArrRealisation(int arrLength) {
-        this.length = arrLength;
-        arr = (T[]) new Object[arrLength];
+    public BinaryTreeForArrRealisation() {
     }
 
     public void setRoot(T root) {
         arr[0] = root;
-        TreeNode rootNode = new TreeNode(root);
-        this.root = rootNode;
-        length++;
     }
 
 
-    public void addLeftChild(int rootIndex, T key) {
+    public void addLeftChild(T elementToAdd, T element) {
+        int rootIndex = getIndexOfEl(elementToAdd);
         int leafIndex = (rootIndex * 2) + 1;
+        if (rootIndex == -1) {
+            System.out.println("Element to add wasn't found");
+            return;
+        }
         if (arr[rootIndex] == null)
             System.out.println("There is no parent for this child");
         else {
-            arr[leafIndex] = key;
-            TreeNode leftChild = new TreeNode(key);
-            this.root.leftChild = leftChild;
-            length++;
+            if (leafIndex >= arr.length) {
+                T[] newArr = (T[]) new Object[arr.length + leafIndex];
+                System.arraycopy(arr, 0, newArr, 0, arr.length);
+                arr = newArr;
+                arr[leafIndex] = element;
+            } else
+                arr[leafIndex] = element;
         }
     }
 
-    public void addRightChild(int rootIndex, T key) {
+    public void addRightChild(T elementToAdd, T element) {
+        int rootIndex = getIndexOfEl(elementToAdd);
         int leafIndex = (rootIndex * 2) + 2;
+        if (rootIndex == -1) {
+            System.out.println("Element to add wasn't found");
+            return;
+        }
         if (arr[rootIndex] == null)
             System.out.println("There is no parent for this child");
         else {
-            arr[leafIndex] = key;
-            TreeNode rightChild = new TreeNode(key);
-            this.root.rightChild = rightChild;
-            length++;
+            if (leafIndex >= arr.length) {
+                T[] newArr = (T[]) new Object[arr.length + leafIndex];
+                System.arraycopy(arr, 0, newArr, 0, arr.length);
+                arr = newArr;
+                arr[leafIndex] = element;
+            } else
+                arr[leafIndex] = element;
+
         }
     }
-
     @Override
     public String toString() {
-        String res = toString(root);
+        String res = toString(0);
         return "(" + res + ")";
     }
 
-    private String toString(TreeNode parent) {
+    private String toString(int rootIndex) {
         String res = "";
-        res += parent.data;
-        if (parent.leftChild != null) {
-            res += "(" + toString(parent.leftChild) + ",";
-            if (parent.rightChild == null)
-                res += "*)";
-        }
-        if (parent.rightChild != null) {
-            if (parent.leftChild == null)
-                res += "(*,";
-            res += toString(parent.rightChild) + ")";
-        }
-        return res;
+        if (arr[rootIndex] == null)
+            return res += "*"; // * is used for empty child
+        if ((rootIndex * 2 + 1 > arr.length || rootIndex * 2 + 2 > arr.length) ||
+                (arr[rootIndex * 2 + 1] == null && arr[rootIndex * 2 + 2] == null))
+            return res += arr[rootIndex];
+        return arr[rootIndex] + "(" + toString((rootIndex * 2) + 1) + "," +
+                toString((rootIndex * 2) + 2) + ")";
     }
+
+    private int getIndexOfEl(T element) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element)
+                return index = i;
+        }
+        return index;
+    }
+
 }
--- Kucherenko_ASD_11_Practice/src/BinaryTreeFromArr.java
+++ Kucherenko_ASD_11_Practice/src/BinaryTreeFromArr.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
-
 public class BinaryTreeFromArr {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of the array you want to get a binary tree from: ");
-        int length = sc.nextInt();
-        BinaryTreeForArrRealisation<String> binaryTree = new BinaryTreeForArrRealisation<>(length);
-        binaryTree.setRoot("A");
-        //    obj.set_Left("B", 0);
-        binaryTree.addRightChild(0, "D");
-        binaryTree.addLeftChild(0, "C");
-        binaryTree.addLeftChild(1, "E");
-        binaryTree.addRightChild(1, "F");
-        System.out.println(binaryTree.toString());
+        BinaryTreeForArrRealisation<Integer> binaryTree = new BinaryTreeForArrRealisation<>();
+        binaryTree.setRoot(1);
+        binaryTree.addRightChild(1, 3);
+        binaryTree.addLeftChild(1, 4);
+        binaryTree.addLeftChild(3, 5);
+        binaryTree.addRightChild(4, 9);
+        System.out.println("Your array implementation of binary tree: " + binaryTree);
     }
 }
