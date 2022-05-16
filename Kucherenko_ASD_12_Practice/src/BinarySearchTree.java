25483c052953a0665fc5ea64c839cb2432a4dfa2
dan_kucherenko <daniil.ukraine@gmail.com>
2022-05-16 23:15:46 +0300

    First commit

---
+++ Kucherenko_ASD_12_Practice/src/BinarySearchTree.java
@@ -0,0 +1,56 @@
+public class BinarySearchTree<T extends Comparable<T>>  {
+    private TreeNode<T> root;
+    private int length;
+
+    public BinarySearchTree(T rootValue) {
+        int exceptions = 0;
+        try {
+            if (rootValue == null) {
+                exceptions++;
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        if (exceptions == 0) {
+            this.root = new TreeNode<>(rootValue);
+            length++;
+        }
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public boolean contains(T element) {
+        boolean isPresent = false;
+        return isPresent;
+    }
+
+    public void add(T element) {
+        addRecursive(root, element);
+    }
+
+    public String toString() {
+        String res = toString(root);
+        return "(" + res + ")";
+    }
+
+    private String toString(TreeNode<T> root) {
+        String res = "";
+        if (root == null)
+            return res += "*"; // * is used for empty child
+        if (root.leftChild == null && root.rightChild == null)
+            return res += root.data;
+        return root.data.toString() + "(" + toString(root.leftChild) + "," + toString(root.rightChild) + ")";
+    }
+    private TreeNode<T> addRecursive(TreeNode<T> nodeToStart, T element) {
+        if (nodeToStart != null) {
+            if (element.compareTo(nodeToStart.data) < 0)
+                nodeToStart.leftChild = addRecursive(nodeToStart.leftChild, element);
+            else if (element.compareTo(nodeToStart.data) > 0)
+                nodeToStart.rightChild = addRecursive(nodeToStart.rightChild, element);
+        }
+        return nodeToStart;
+    }
+}
---
+++ Kucherenko_ASD_12_Practice/src/TesterBST.java
@@ -0,0 +1,8 @@
+public class TesterBST {
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree(1);
+        binarySearchTree.add(2);
+        binarySearchTree.add(3);
+        System.out.println(binarySearchTree);
+    }
+}
