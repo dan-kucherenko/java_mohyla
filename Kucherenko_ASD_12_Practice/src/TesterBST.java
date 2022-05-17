5a5410f43ba6e343ae5c77ff4f4b29a224a2f628
dan_kucherenko <daniil.ukraine@gmail.com>
2022-05-17 15:04:13 +0300

    BinarySearchTree with exceptions added

--- Kucherenko_ASD_12_Practice/src/TesterBST.java
+++ Kucherenko_ASD_12_Practice/src/TesterBST.java
@@ -1,5 +1,8 @@
+import java.util.Locale;
+
 public class TesterBST {
     public static void main(String[] args) {
+        System.out.println("Testing default binary search tree:\n".toUpperCase(Locale.ROOT));
         BinarySearchTree<String> binarySearchTree = new BinarySearchTree<>();
         System.out.println("Your binary search tree is empty: " + binarySearchTree.isEmpty());
         binarySearchTree.add("Boris");
@@ -14,5 +17,15 @@ public class TesterBST {
         System.out.println("Your binary search tree is empty: " + binarySearchTree.isEmpty());
         System.out.println("Your binary search tree contains the inserted element: " +
                 binarySearchTree.contains("Kevin"));
+
+        System.out.println("\nTesting binary search tree with exceptions:\n".toUpperCase(Locale.ROOT));
+        BinarySearchTree<Integer> binarySearchTreeWithExceptions = new BinarySearchTree<>();
+        System.out.println("Your binary search tree is empty: " + binarySearchTreeWithExceptions.isEmpty());
+        binarySearchTreeWithExceptions.add(null); //throws NUllPointerException
+        System.out.println(binarySearchTreeWithExceptions);
+        System.out.println("Your binary search tree is empty: " + binarySearchTreeWithExceptions.isEmpty());
+        System.out.println("Your binary search tree contains the inserted element: " +
+                binarySearchTreeWithExceptions.contains(null)); //throws NUllPointerException
+
     }
 }
